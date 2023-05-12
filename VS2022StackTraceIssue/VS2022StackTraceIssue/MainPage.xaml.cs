b8be260363d8781f20fe4f1b63c0effdc6072649
Oliver Spencer <email@oliverspencer.com>
2023-05-12 22:40:58 +0100

    Initial commit

---
+++ VS2022StackTraceIssue/VS2022StackTraceIssue/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace VS2022StackTraceIssue
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            object m = null;
+            string x = m.ToString();
+        }
+    }
+}
+
